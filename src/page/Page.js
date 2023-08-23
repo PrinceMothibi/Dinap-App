93c8ef4d5764b5d77dc5fa8da3289387fb883f61
root <root@ip-172-31-10-91.us-east-2.compute.internal>
2023-08-23 06:34:23 +0000

    initial commit

---
+++ src/page/Page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './footer/Footer.js';
+import Header from './header/Header.js';
+
+const Page =() => {
+	return (
+		<div>
+		    <Header/>
+		    <Footer/>
+		</div>
+	);
+};
+
+export default Page;
---
+++ src/page/header/logo_banner/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Logo = () =>{
+	return(
+		<div className ='logo-div'>
+		    
+		    <p>image here</p>
+		    
+		    
+		    <h1 className="company-text">Dinao B2B</h1>
+		</div>
+	);
+};
+
+export default Logo;
