14011c35ee5e3a4e05b1825798a40d3cc0af16ff
Ofentse <ofmothi022@student.wethinkcode.co.za>
2023-08-28 21:33:52 +0200

    added page components

--- src/page/header/Header.js
+++ src/page/header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Header.css';
 import MenuButton from './menu_button/MenuButton.js';
 import Logo from './logo_banner/Logo.js';
 import Account from './account/Account.js';
@@ -6,11 +7,9 @@ import Account from './account/Account.js';
 const Header = () => {
   return (
     <header className="header">
-      <div className="header-content">
         <MenuButton />
         <Logo />
         <Account />
-      </div>
     </header>
   );
 };
