a724e87300b9292f5be03ab486f2d3e3dc5c10bb
Ofentse <ofmothi022@student.wethinkcode.co.za>
2023-08-28 21:33:52 +0200

    added page components

--- src/App.js
+++ src/App.js
@@ -5,23 +5,6 @@ import Page from './page/Page.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-	  <div>
-	      <p>This is the centre</p>
-	  </div>
 	  <Page/>
     </div>
   );
--- src/page/Page.js
+++ src/page/Page.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from './footer/Footer.js';
 import Header from './header/Header.js';
+import Content from './content/Content.js';
 
 const Page =() => {
 	return (
 		<div>
 		    <Header/>
+			<Content/>
 		    <Footer/>
 		</div>
 	);
