cd60c61cc4c801c121614a98e5ee609296264ae5
Ofentse <ofmothi022@student.wethinkcode.co.za>
2023-09-01 12:42:42 +0200

    added multiple pages

---
+++ src/header/logo_banner/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () =>{
+	return(
+		<div className ='logo-div'> 
+		    <img src='https://wwwdinaostored0b2f.zapwp.com/q:i/r:0/wp:1/w:1/u:https://www.dinao.store/wp-content/uploads/2022/10/dinao-store-final-logo.png' alt='Dinao Store Logo' />
+		</div>
+	);
+};
+
+export default Logo;
--- src/page/Page.js
+++ src/page/Page.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import Footer from './footer/Footer.js';
-import Header from './header/Header.js';
 import Content from './content/Content.js';
 
 const Page =() => {
 	return (
-		<div>
-		    <Header/>
-			<Content/>
-		    <Footer/>
-		</div>
+		<Content/>
 	);
 };
 
