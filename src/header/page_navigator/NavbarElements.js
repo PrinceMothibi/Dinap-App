1e8d67f4f22276fdbe0a09d56b9821f50d38faa1
Ofentse Mothibi <ofmothi022@student.wethinkcode.co.za>
2023-09-04 16:11:02 +0200

    added styling to div

--- src/header/page_navigator/NavbarElements.js
+++ src/header/page_navigator/NavbarElements.js
@@ -12,7 +12,7 @@ z-index: 12;
 `;
 
 export const NavLink = styled(Link)`
-color: #808080;
+color: #000000;
 display: flex;
 align-items: center;
 text-decoration: none;
@@ -20,7 +20,7 @@ padding: 0 1rem;
 height: 100%;
 cursor: pointer;
 &.active {
-	color: #4d4dff;
+	color: #808080;
 }
 `;
 
