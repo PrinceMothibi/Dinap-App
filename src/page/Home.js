05306be37fcf4905e1161d7ee6b6cefc2c3fc03f
Ofentse <ofmothi022@student.wethinkcode.co.za>
2023-09-08 15:14:45 +0200

    removed amplify file

--- src/page/Home.js
+++ src/page/Home.js
@@ -17,7 +17,7 @@ const Home =() =>{
                     By providing high quality, world-class equipment affordably.
                     
                 </p>
-                <img src="https://images.zapnito.com/cdn-cgi/image/metadata=copyright,fit=scale-down,format=auto,quality=95/https://images.zapnito.com/users/437596/posters/1600283129-58-2219/40dab507-fb9e-423e-bd14-3041cd4cbe7b_large.jpeg"></img>
+                <img src="https://images.zapnito.com/cdn-cgi/image/metadata=copyright,fit=scale-down,format=auto,quality=95/https://images.zapnito.com/users/437596/posters/1600283129-58-2219/40dab507-fb9e-423e-bd14-3041cd4cbe7b_large.jpeg" alt="Monkey business"></img>
             </section>
         </React.Fragment>
     )
