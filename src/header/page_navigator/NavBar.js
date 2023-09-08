184251bb83b4db72d1e13050998520b91d47d058
Ofentse <ofmothi022@student.wethinkcode.co.za>
2023-09-08 13:04:44 +0200

    added authenticated pages

--- src/header/page_navigator/NavBar.js
+++ src/header/page_navigator/NavBar.js
@@ -1,31 +1,51 @@
 import React from "react";
-import { Nav, NavLink, NavMenu} from "./NavbarElements";
+import { Nav, NavLink, NavMenu } from "./NavbarElements";
 import Logo from "../logo_banner/Logo";
+import {useAuthenticator, Button } from "@aws-amplify/ui-react";
 
 const Navbar = () => {
+  function logOut() {
+    signOut();
     return (
-        <>
-            <Nav>
-                <Logo />
-                <NavMenu>
-                    <NavLink to="/" activeStyle>
-                        Home</NavLink>
-                    <NavLink to="/about" activeStyle>
-                        About
-                    </NavLink>
-                    <NavLink to="/brands" activeStyle>
-                        Brands
-                    </NavLink>
-                    <NavLink to="/shop" activeStyle>
-                        Shop
-                    </NavLink>
-                    <NavLink to="/account" activeStyle>
-                        Account
-                    </NavLink>
-                </NavMenu>
-            </Nav>
-        </>
+      <NavLink to="/account" activeStyle>
+        Logout
+      </NavLink>
     );
+  }
+
+  const { route, signOut } = useAuthenticator((context) => [
+    context.route,
+    context.signOut,
+  ]);
+
+  return (
+    <>
+      <Nav>
+        <Logo />
+        <NavMenu>
+          <NavLink to="/" activeStyle>
+            Home
+          </NavLink>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/brands" activeStyle>
+            Brands
+          </NavLink>
+          <NavLink to="/shop" activeStyle>
+            Shop
+          </NavLink>
+          {route !== "authenticated" ? (
+            <NavLink to="/account" activeStyle>
+              Login
+            </NavLink>
+          ) : (
+            <Button onClick={() => logOut()}>Logout</Button>
+          )}
+        </NavMenu>
+      </Nav>
+    </>
+  );
 };
- 
+
 export default Navbar;
--- src/page/Account.js
+++ src/page/Account.js
@@ -4,16 +4,15 @@ import LoginPage from "./account-pages/Login";
 import LogoutPage from "./account-pages/Logout";
 import AccountNav from "./account-pages/AccountNavigator";
 
-const Account=()=>{
-    return(
-        <React.Fragment>
-            <div>
-                <LoginPage/>
-                <LogoutPage/>
-            </div>
+const Account = () => {
+  return (
+    <React.Fragment>
+      <div>
+        <LoginPage />
+        <LogoutPage />
+      </div>
     </React.Fragment>
-    );
-    
+  );
 };
 
 export default Account;
