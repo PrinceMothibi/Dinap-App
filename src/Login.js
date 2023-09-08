cd012f4a70b2d3aa81c1bef6ab161ae2a85ce07f
Ofentse <ofmothi022@student.wethinkcode.co.za>
2023-09-08 15:50:28 +0200

    changed authenticator name

--- src/Login.js
+++ src/Login.js
@@ -3,12 +3,12 @@ import { useEffect } from "react";
 import { Authenticator, useAuthenticator, View } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { useNavigate, useLocation } from 'react-router';
-import awsconfig from "./aws-exports";
+import awsmobile from "./aws-exports";
 import { Amplify } from "aws-amplify";
 
 
 export default function Login() {
-  Amplify.configure(awsconfig);
+  Amplify.configure(awsmobile);
   const { route } = useAuthenticator((context) => [context.route]);
   const location = useLocation();
   const navigate = useNavigate();
